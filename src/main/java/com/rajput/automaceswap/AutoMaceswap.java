16d0b0b40ee264836cb5af1f0ccae400b139b6a5
Your Name <you@example.com>
2026-04-02 18:33:34 +0530

    Add base ModInitializer to pass build

---
+++ src/main/java/com/rajput/automaceswap/AutoMaceswap.java
@@ -0,0 +1,10 @@
+package com.rajput.automaceswap;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AutoMaceswap implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("AutoMaceswap initialized!");
+    }
+}
