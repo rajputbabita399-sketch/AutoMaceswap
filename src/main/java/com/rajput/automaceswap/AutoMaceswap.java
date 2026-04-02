b5ceabc93e999d4f35151cd680db6285b1703bff
Your Name <you@example.com>
2026-04-02 18:43:13 +0530

    Force Java 21 and fix directory structure

--- src/main/java/com/rajput/automaceswap/AutoMaceswap.java
+++ src/main/java/com/rajput/automaceswap/AutoMaceswap.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class AutoMaceswap implements ModInitializer {
     @Override
     public void onInitialize() {
-        System.out.println("AutoMaceswap initialized!");
+        System.out.println("AutoMaceswap is ready for Wurst integration!");
     }
 }
